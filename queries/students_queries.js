8f6dcad99b93234a683a12c5f5eac00414608be8
daneparke <dapa8522@colorado.edu>
2018-12-05 11:53:14 -0700

    added create student query and route, working

--- queries/students_queries.js
+++ queries/students_queries.js
@@ -6,6 +6,9 @@ module.exports = {
     },
     readStudentById(id) {
         return db('students').where({ id: id }).first()
+    },
+    createStudent(newStudent) {
+        return db('students').insert(newStudent).returning('*')
     }
 
 }
--- routes/students_routes.js
+++ routes/students_routes.js
@@ -10,4 +10,18 @@ router.get('/:id', (req, res) => {
     queries.readStudentById(req.params.id).then(student => res.status(200).send(student))
 })
 
+router.post('/', (req, res) => {
+    queries.createStudent(req.body).then(newStudent => res.status(201).send(newStudent))
+})
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not found.' })
+})
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({ error: err })
+})
+
+
 module.exports = router
